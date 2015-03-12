cf641622acbc52999122c0db75cfc5f8c9a290d7
mrgeiby <mike@mrgeiby.co.uk>
2015-03-12 17:15:22 +0000

    Update currency symbol on basket to pounds

--- resources/views/basket/index.blade.php
+++ resources/views/basket/index.blade.php
@@ -70,12 +70,12 @@
                                     <div class="row">
                                         <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
                                         <div class="col-sm-10">
-                                            <h4 class="nomargin">Product 1</h4>
+                                            <h4 class="nomargin">Performance</h4>
                                             <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
                                         </div>
                                     </div>
                                 </td>
-                                <td data-th="Price">$1.99</td>
+                                <td data-th="Price">£1.99</td>
                                 <td data-th="Quantity">
                                     <input type="number" class="form-control text-center" value="1">
                                 </td>
@@ -92,9 +92,9 @@
                                 <td class="text-center"><strong>Total 1.99</strong></td>
                             </tr>
                             <tr>
-                                <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+                                <td><a href="#" class="btn btn-primary"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
                                 <td colspan="2" class="hidden-xs"></td>
-                                <td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+                                <td class="hidden-xs text-center"><strong>Total £1.99</strong></td>
                                 <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
                             </tr>
                             </tfoot>
