6c1b9ed23e01898a416a018121cd3d2598ceb8c7
mrgeiby <mike@mrgeiby.co.uk>
2015-03-12 10:46:42 +0000

    Update migration class name

--- database/migrations/2015_03_07_151812_update_productions_table.php
+++ database/migrations/2015_03_07_151812_update_productions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateProductionsTable extends Migration {
+class UpdateProductionsTable2 extends Migration {
 
     /**
      * Run the migrations.
