1db82040d61135edc5d3145b811c259c11b4127e
mrgeiby <mike@mrgeiby.co.uk>
2015-03-15 22:06:18 +0000

    Add zero results condition to productions searchResult view

--- resources/views/production/searchResult.blade.php
+++ resources/views/production/searchResult.blade.php
@@ -1,9 +1,13 @@
-@foreach ($data as $production)
-    <ul>
-        <h3>{!! HTML::linkAction('ProductionController@show', $production->prodName,
-            $production->prodSlug) !!}</h3>
-        <i>Released: {{ $production->created_at }}</i>
+@if ($data->count() == 0)
+    <p>No production was found.</p>
+@else
+    @foreach ($data as $production)
+        <ul>
+            <h3>{!! HTML::linkAction('ProductionController@show', $production->prodName,
+                $production->prodSlug) !!}</h3>
+            <i>Released: {{ $production->created_at }}</i>
 
-        <p>{{ $production->prodDescription }}</p>
-    </ul>
-@endforeach
+            <p>{{ $production->prodDescription }}</p>
+        </ul>
+    @endforeach
+@endif
